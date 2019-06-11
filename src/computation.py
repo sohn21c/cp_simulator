fc529ed00e5926b5549bd868988fd84302f7c159
James S <sohn21c@hotmail.com>
2019-06-11 10:40:43 -0500

    remove unused helper functions and verified the algorithm

--- src/computation.py
+++ src/computation.py
@@ -20,26 +20,13 @@ fps = 200
 
 def R_2vect(vector_orig, vector_fin):
     """Calculate the rotation matrix required to rotate from one vector to another.
+	
+	input: 
+		- vector_orig
+		- vector_fin
 
-    For the rotation of one vector to another, there are an infinit series of rotation matrices possible.  Due to axially symmetry, the rotation axis can be any vector lying in the symmetry
-    plane between the two vectors.  Hence the axis-angle convention will be used to construct the
-    matrix with the rotation axis defined as the cross product of the two vectors.  The rotation
-    angle is the arccosine of the dot product of the two unit vectors.
-
-    Given a unit vector parallel to the rotation axis, w = [x, y, z] and the rotation angle a,
-    the rotation matrix R is::
-
-              |  1 + (1-cos(a))*(x*x-1)   -z*sin(a)+(1-cos(a))*x*y   y*sin(a)+(1-cos(a))*x*z |
-        R  =  |  z*sin(a)+(1-cos(a))*x*y   1 + (1-cos(a))*(y*y-1)   -x*sin(a)+(1-cos(a))*y*z |
-              | -y*sin(a)+(1-cos(a))*x*z   x*sin(a)+(1-cos(a))*y*z   1 + (1-cos(a))*(z*z-1)  |
-
-
-    @param R:           The 3x3 rotation matrix to update.
-    @type R:            3x3 numpy array
-    @param vector_orig: The unrotated vector defined in the reference frame.
-    @type vector_orig:  numpy array, len 3
-    @param vector_fin:  The rotated vector defined in the reference frame.
-    @type vector_fin:   numpy array, len 3
+	output:
+		- 3x3 rotation matrix that rotate vector_orig to vector_fin
     """
 
     # Create empty rotation matrix
@@ -86,16 +73,28 @@ def SORA(w_x, w_y, w_z):
 	finds the rotation matrix at fixed axis with angle from orthogonal angular velocity measurement
 
 	Reference: https://www.hindawi.com/journals/js/2018/9684326/
+
+	input:
+		- w_x, w_y, w_z: constant angular velocity at give period w.r.t each 	axis in 3d
+
+	output:
+		- 3x3 rotation matrix from initial coordinate to final coordinate
 	"""
 	# define the array of w
 	w = np.array([w_x, w_y, w_z])
 
+	# scale up the w to avoid forcing norm to be zero
+	w *= 1e6
+
 	# if norm of the angular velocity is too small, return identity matrix
-	if np.linalg.norm(w) < 1e-3:
+	if np.linalg.norm(w) < 1e-12:
 		return np.identity(3)
 
 	# find the fixed rotation axis from w
 	v_x, v_y, v_z = w / np.linalg.norm(w)
+
+	# scale back down the w
+	w /= 1e6
 
 	# calculate the amount of rotation psi, norm of angluar velocity multiplied by the period of measurement
 	psi = np.linalg.norm(w) * (1.0/fps)
@@ -113,115 +112,6 @@ def SORA(w_x, w_y, w_z):
 	R[2,2] = np.cos(psi) + v_z**2 * (1-np.cos(psi))
 
 	return R
-
-# rotation matrix of vector at fixed axis with angle
-def rotation_mat(u, w):
-	"""
-	- Matrix logarithm
-	finds the rotation matrix at fixed axis of u with angle w
-	"""
-	# extract vector in each axis from u
-	u_x = u[0]
-	u_y = u[1]
-	u_z = u[2]
-
-	# create empty container
-	Rot = np.zeros([3,3])
-
-	# compute the elements
-	Rot[0,0] = np.cos(w) + u_x**2 * (1-np.cos(w))
-	Rot[0,1] = u_x * u_y * (1-np.cos(w)) - u_z * np.sin(w)
-	Rot[0,2] = u_x * u_z * (1-np.cos(w)) + u_y * np.sin(w)
-	Rot[1,0] = u_x * u_y * (1-np.cos(w)) + u_z * np.sin(w)
-	Rot[1,1] = np.cos(w) + u_y**2 * (1-np.cos(w))
-	Rot[1,2] = u_y * u_z * (1-np.cos(w)) - u_x * np.sin(w)
-	Rot[2,0] = u_x * u_z * (1-np.cos(w)) - u_y * np.sin(w)
-	Rot[2,1] = u_y * u_z * (1-np.cos(w)) + u_x * np.sin(w)
-	Rot[2,2] = np.cos(w) + u_z**2 * (1-np.cos(w))
-
-	return Rot
-
-# transformation matrix
-def transformation_mat(a, b, c, A, B, C):
-	"""
-	finds transformation matrix between two cartesian coordinate
-	"""
-	Trans = np.zeros([3,3])
-	Trans[0,0] = np.dot(a, A)
-	Trans[0,1] = np.dot(a, B)
-	Trans[0,2] = np.dot(a, C)
-	Trans[1,0] = np.dot(b, A)
-	Trans[1,1] = np.dot(b, B)
-	Trans[1,2] = np.dot(b, C)
-	Trans[2,0] = np.dot(c, A)
-	Trans[2,1] = np.dot(c, B)
-	Trans[2,2] = np.dot(c, C)
-
-	return Trans
-
-##### test module for pose_only
-def pose_only(filename):
-	"""
-	test module to integrate the acceleartion only to test the behaviour
-	"""
-	# retrieve the data from the data_parser module
-	acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z = data_parser.data_parser(filename)
-
-	# create the data index 
-	ind = 0
-
-	# initial gravity at t = 0
-	acc_x_init = acc_x[0]
-	acc_y_init = acc_y[0]
-	acc_z_init = acc_z[0]
-
-	# compute gravity
-	init_gravity = np.sqrt(acc_x_init**2 + acc_y_init**2 + acc_z_init**2)
-
-	# gravity in z direction in world frame
-	gravity_worldframe = np.array([0, 0, init_gravity])
-
-	# subtract the gravity from the acceleration in body frame
-	for i in range(len(acc_x)):
-		acc_x[i] -= acc_x_init
-		acc_y[i] -= acc_y_init
-		acc_z[i] -= acc_z_init
-
-	# plot for check
-	plot_x = np.linspace(0, 100, len(acc_x))
-	plt.plot(plot_x, acc_x, 'r')
-	plt.plot(plot_x, acc_y, 'g')
-	plt.plot(plot_x, acc_z, 'b')
-	plt.title("acceleartion")
-	plt.show()
-
-	# integrate acceleration for velocity
-	vel_x = np.cumsum(acc_x, axis = 0)
-	vel_y = np.cumsum(acc_y, axis = 0)
-	vel_z = np.cumsum(acc_z, axis = 0)
-
-	# plot for check
-	plot_x = np.linspace(0, 100, len(acc_x))
-	plt.plot(plot_x, vel_x, 'r')
-	plt.plot(plot_x, vel_y, 'g')
-	plt.plot(plot_x, vel_z, 'b')
-	plt.title("velocity")
-	plt.show()
-
-	# integrate velocity for position
-	pos_x = np.cumsum(vel_x, axis = 0)
-	pos_y = np.cumsum(vel_y, axis = 0)
-	pos_z = np.cumsum(vel_z, axis = 0)
-	
-	# plot for check
-	plot_x = np.linspace(0, 100, len(acc_x))
-	plt.plot(plot_x, pos_x, 'r')
-	plt.plot(plot_x, pos_y, 'g')
-	plt.plot(plot_x, pos_z, 'b')
-	plt.title("position")
-	plt.show()
-
-	return pos_x, pos_y, pos_z
 
 def pos_rot_calculation(filename):
 	"""
@@ -260,7 +150,7 @@ def pos_rot_calculation(filename):
 	init_gravity = np.sqrt(acc_x_init**2 + acc_y_init**2 + acc_z_init**2)
 
 	# gravity in z direction in world frame
-	gravity_worldframe = np.array([0, 0, -init_gravity])
+	gravity_worldframe = np.array([0., 0., -init_gravity])
 
 	# initial gravity in body frame
 	init_gravity_bodyframe = np.array([acc_x_init, acc_y_init, acc_z_init])
@@ -268,14 +158,14 @@ def pos_rot_calculation(filename):
 	# find rotation matrix rotating bodyframe to world frame
 	RR = R_2vect(init_gravity_bodyframe, gravity_worldframe)
 
-	# world frame vectors
-	x_abs = np.array([1, 0, 0])
-	y_abs = np.array([0, 1, 0])
-	z_abs = np.array([0, 0, 1])
-	coord_unit = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+	# frame
+	x_abs = np.array([1., 0., 0.])
+	y_abs = np.array([0., 1., 0.])
+	z_abs = np.array([0., 0., 1.])
+	identity_coord_body = np.array([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.]])
 
 	# find sensor coordinate in world frame
-	coord_sensor = np.dot(RR, coord_unit)
+	sensor_coord_world = np.dot(RR, identity_coord_body)
 
 	# define the initial velocity in world frame
 	vel_world = np.array([0., 0., 0.])
@@ -287,6 +177,15 @@ def pos_rot_calculation(filename):
 	posx_time = []
 	posy_time = []
 	posz_time = []
+	rot_time_00 = []
+	rot_time_01 = []
+	rot_time_02 = []
+	rot_time_10 = []
+	rot_time_11 = []
+	rot_time_12 = []
+	rot_time_20 = []
+	rot_time_21 = []
+	rot_time_22 = []
 
 	# iterate through the measurement and update the position and orientation of the frame
 	for i in range(len(acc_x)):
@@ -296,7 +195,7 @@ def pos_rot_calculation(filename):
 		# rotate the body frame acceleartion to world frame
 		acc_world = np.dot(RR, acc_body)
 		
-		# subtract the gravitational acceleration
+		# subtract the gravitational acceleration. g in -z axis
 		acc_world -= np.array([0, 0, -init_gravity])
 
 		# integerate acceleartion to get velocity in world frame
@@ -309,24 +208,42 @@ def pos_rot_calculation(filename):
 		Rot_body = SORA(gyro_x[i], gyro_y[i], gyro_z[i])
 
 		# find the next body frame with the Rot_body
-		coord_sensor = np.dot(coord_sensor, Rot_body)
+		sensor_coord_world = np.dot(sensor_coord_world, Rot_body)
+	
+		# print statement for intermediate value check
+		if i % 20 == 0:
+			# print(acc_world)
+			continue
 
 		# update the Rotation matrix for world frame representation
-		# RR = np.dot(RR, Rot_body)
+		RR = np.dot(RR, Rot_body)
 
 		# append to container
 		posx_time.append(pos_world[0])
 		posy_time.append(pos_world[1])
 		posz_time.append(pos_world[2])
+		rot_time_00.append(sensor_coord_world[0,0])
+		rot_time_01.append(sensor_coord_world[0,1])
+		rot_time_02.append(sensor_coord_world[0,2])
+		rot_time_10.append(sensor_coord_world[1,0])
+		rot_time_11.append(sensor_coord_world[1,1])
+		rot_time_12.append(sensor_coord_world[1,2])
+		rot_time_20.append(sensor_coord_world[2,0])
+		rot_time_21.append(sensor_coord_world[2,1])
+		rot_time_22.append(sensor_coord_world[2,2])
 
 	# plot for check
-	# plot_x = np.linspace(0, 100, len(posx_plot))
-	# plt.plot(plot_x, posx_plot, 'r')
-	# plt.plot(plot_x, posy_plot, 'g')
-	# plt.plot(plot_x, posz_plot, 'b')
-	# plt.show()
+	plot_x = np.linspace(0, 100, len(posx_time))
+	plt.plot(plot_x, posx_time, 'r', label='x')
+	plt.plot(plot_x, posy_time, 'g', label='y')
+	plt.plot(plot_x, posz_time, 'b', label='z')
+	plt.title("Position", loc='center')
+	plt.legend(loc='upper left')
+	plt.ylabel("[m]")
+	plt.xlabel("Relative time")
+	plt.show()
 
-	return posx_time, posy_time, posz_time
+	return posx_time, posy_time, posz_time, rot_time_00, rot_time_01, rot_time_02, rot_time_10, rot_time_11, rot_time_12, rot_time_20,rot_time_21, rot_time_22
 
 if __name__ == '__main__':
 	try:
@@ -334,13 +251,20 @@ if __name__ == '__main__':
 		filename = input("> ")
 		
 		# publishes data from the file
-		posx_time, posy_time, posz_time = pos_rot_calculation(filename)
+		posx_time, posy_time, posz_time, \
+		rot_time_00, rot_time_01, rot_time_02, \
+		rot_time_10, rot_time_11, rot_time_12, \
+		rot_time_20,rot_time_21, rot_time_22 = pos_rot_calculation(filename)
 
 		# write the csv file
 		with open('pose.csv', mode='w') as csvwriter:
 			writer = csv.writer(csvwriter, delimiter = ',')
 			for i in range(len(posx_time)):
-				writer.writerow([posx_time[i], posy_time[i], posz_time[i]])
+				writer.writerow(
+					[posx_time[i], posy_time[i], posz_time[i], 
+					rot_time_00[i], rot_time_01[i], rot_time_02[i], 
+					rot_time_10[i], rot_time_11[i], rot_time_12[i], 
+					rot_time_20[i], rot_time_21[i], rot_time_22[i]])
 
 		#### test fn.vector rotation (done)
 		# a_body = np.array([1., 0., 0.])
